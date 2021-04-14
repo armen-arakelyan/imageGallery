6c10730078ffa8fe6a4f4ced556a33d131107b24
armen-arakelyan <arakelyan2001@mail.ru>
2021-04-14 19:28:17 +0400

    redux

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import {imageRequest} from '../redux/imgGallery/action';
 import Cards from './Cards';
@@ -7,32 +7,34 @@ import Loader from './Loader';
 const Gallery=()=>{
     const img=useSelector(state=>state.img);
     const dispatch=useDispatch();
+    const [loader,setLoader]=useState('none');
      useEffect(()=>{
          dispatch(imageRequest());
     },[dispatch])
     const loadfunc=()=>{
-            if(img.loader===true){
-                dispatch(imageRequest(img.loader))
+            if(img.loader){
+                dispatch(imageRequest())
             }
     }
     useEffect(()=>{
         document.addEventListener('scroll',()=>{   
             let height = Math.max( document.body.scrollHeight,null) ;  
             if (window.scrollY+939===height) {
-                loadfunc()
+                setLoader('block')
+                setTimeout(()=>{loadfunc();setLoader('none')},3000)
               }
         })
     })
     return(
         <div className="photo_gallery">
             <h1>Image Gallery</h1>
-            { img.data.length===0 ?<Loader />:
-             img.data.map((v,i)=>{
+             {img.data.length===0?<Loader />:img.data.map((v,i)=>{
                  return <div key={i}>
                      <Cards name={v.author} img={v.download_url} />
                       </div>           
             })
         }
+        <Loader style={{display:loader}} />
         </div>
     )
 }
--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Loader=()=>{
+const Loader=(props)=>{
     return(
-        <div className="loader">
+        <div style={props.style} className="loader">
           </div>
     )
 }
